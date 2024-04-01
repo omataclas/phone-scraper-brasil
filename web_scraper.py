dbd92dfb53ae7b1873dd23c2b0c0e18ecf1b68a8
omataclas <omataclas@gmail.com>
2024-04-01 18:07:33 -0300

    Removed all references to "email" functionalities

--- web_scraper.py
+++ web_scraper.py
@@ -15,7 +15,7 @@ from datetime import datetime
 from openpyxl import Workbook
 from bs4 import BeautifulSoup
 
-save_excel = True #Change to "True" to save email into Excel
+save_excel = True #Change to "True" to save phone number into Excel
 
 book = Workbook()
 sheet = book.active
@@ -29,18 +29,14 @@ def start_scrape(page, name_the_file):
     scrape = scrape.get_text()
     
     phone_numbers = set(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", scrape)) #"set" removes duplicates
-    emails = set(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", scrape))
 
     nodupnumber = len(list(phone_numbers))
-    nodupemail = len(list(emails))
 
     dupnumber = len(list(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", scrape))) 
-    dupemail = len(list(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", scrape)))
 
     number_of_dup_number = int(dupnumber) - int(nodupnumber) 
-    number_of_dup_email = int(dupemail) - int(nodupemail)
 
-    email_list = list(emails)
+    phone_list = list(phone_numbers)
 
     if len(phone_numbers) == 0:
         print("No phone number(s) found.")
@@ -52,28 +48,16 @@ def start_scrape(page, name_the_file):
             print("Phone number #" + str(count) + ': ' + item)
             count += 1
 
-    print("-----------------------------\n")
-
-    if len(emails) == 0:
-        print("No email address(es) found.")
-        print("-----------------------------\n")
-    else:
-        count = 1
-        for item in emails:
-            print('Email address #' + str(count) + ': ' + item)
-            count += 1
-
+    
     if save_excel:
-        for row in zip(email_list):
+        for row in zip(phone_list):
             sheet.append(row)
         excel_file = (name_the_file + ".xlsx")
         book.save(excel_file) 
        
     print("\nDuplicates have been removed from list.")
     print("Total phone numbers: ", nodupnumber)
-    print("Total email addresses: ", nodupemail)
     print("There were " + str(number_of_dup_number) + " duplicate phone numbers.")
-    print("There were " + str(number_of_dup_email) + " duplicate email addresses.")
 
     if save_excel:
         print("\n\nData has been stored inside of an Excel spreadsheet named: "
